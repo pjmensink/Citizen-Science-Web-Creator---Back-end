522f1c28700fe3662678755445786c88003427af
DomGarg <40248050+DomGarg@users.noreply.github.com>
2020-01-15 01:30:47 -0500

    Update server.js

--- src/lib/server.js
+++ src/lib/server.js
@@ -8,13 +8,6 @@ const errorHandler = require('../_helpers/error-handler');
 const fileUpload = require('express-fileupload');
 path = require('path')
 
-//res.header("Access-Control-Allow-Origin", "*");
-
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
