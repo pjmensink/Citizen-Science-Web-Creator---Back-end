f16ca0f9f42c7aba604945f911b3a699dd7e7b6a
DomGarg <40248050+DomGarg@users.noreply.github.com>
2020-01-14 23:51:47 -0500

    Update server.js

--- src/lib/server.js
+++ src/lib/server.js
@@ -41,6 +41,6 @@ app.get("/fish.jpg", (req, res) => {
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-const server = app.listen(port , function(){
+const server = app.listen(process.env.PORT || 3000 , function(){
     console.log('Server listening on port ' + port);
 });
